581b86cde5b93d277a426456b46985fee17b5936
krishna <knikam2025@gmail.com>
2024-06-19 07:43:09 +0530

    added pages

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -11,6 +11,10 @@ import BlogPage from "./BlogPage.jsx";
 
 function Blogs() {
   
+  const [searchTerm, setSearchTerm] = useState("");
+
+
+
   // const [filter, setFilter] = useState()
   // filter((Data) => !selectCategory || Data.category === selectCategory)
 
@@ -31,23 +35,45 @@ function Blogs() {
   // // console.log(filter)
   // // console.log(setFilter)
 
+
+
+
+
+
+
+
+
+
+
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const filteredBlogs = Data.filter(blog => 
+        blog.title.toLowerCase().includes(searchTerm.toLowerCase()) || 
+        blog.category.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
   return (
     <>
-      <div className="h-[auto] w-[100%] bg-gradient-to-tr from-zinc-600 to-zinc-950 flex justify-center items-center flex-col">
-        <h1 className="text-4xl m-5 text-white">There are some latest blogs for you ....</h1>
-        <div className="flex mb-12 relative">
-        <input type="text" placeholder="Search here..."  className="cursor-pointer bg-transparent border-2 border-white rounded-md p-1 text-white outline-none md:w-[20vw]" />
-        <button className="absolute right-0"><FaMagnifyingGlass className="text-white text-2xl m-2 font-bold"/></button>
+        <div className="h-[auto] w-[100%] bg-gradient-to-tr from-zinc-600 to-zinc-950 flex justify-center items-center flex-col">
+            <h1 className="text-4xl m-5 text-white">There are some latest blogs for you ....</h1>
+            <div className="flex mb-12 relative">
+                <input 
+                    type="text" 
+                    placeholder="Search here..." 
+                    className="cursor-pointer bg-transparent border-2 border-white rounded-md p-1 text-white outline-none md:w-[20vw]"
+                    value={searchTerm}
+                    onChange={handleSearchChange}
+                />
+                <button className="absolute right-0"><FaMagnifyingGlass className="text-white text-2xl m-2 font-bold" /></button>
+            </div>
+            <div className="flex justify-center items-center flex-wrap w-[85%] gap-3">
+                <Cards blogs={filteredBlogs} />
+            </div>
         </div>
-        <BlogPage/>
-        <div className="flex justify-center items-center flex-wrap w-[85%] gap-3">
-          <Cards/>
-          <Cards/>
-          <Cards/>
-        </div>
-      </div>
     </>
-  )
+);
 }
 
-export default Blogs
+export default Blogs;
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,40 +2,32 @@
 // import { Link } from "react-router-dom";
 // import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import Data from "../components/Data.js"
 import { FaUser } from "react-icons/fa6";
 
-function Cards({blogs, selectCategory}) {
-    
+function Cards({ blogs }) {
     return (
         <>
-            {
-                Data.splice(0,12).map((val, index) => {
-                    return (
-                        <>
-                            <div id={index} className="card w-64 m-2 shadow-md hover:shadow-gray-200 rounded-2xl">
-                                <img src={val.image} alt="" className="h-1/2 border-t-1 rounded-t-2xl"/>
-
-                                <div className=" bg-white p-2 border-b-0 rounded-b-2xl">
-                                    <h1 className="text-black font-semibold pt-3">{val.title}</h1>
-                                    <span className="flex justify-start items-center pt-2">
-                                    <FaUser />
-                                    <p className="text-left text-xl font-semibold pl-2">{val.author}</p>
-                                    </span>
-                                    <p className=" py-2">Published on : {val.published_date}</p>
-                                    <Link to="/Blogs">Read more....</Link>
-                                </div>
-
-                            </div>
-                        </>
-                    )
-                })
-            }
-
+            {blogs.map((val, index) => (
+                <div key={index} className="card w-64 m-2 shadow-md hover:shadow-gray-200 rounded-2xl">
+                    <img src={val.image} alt={val.title} className="h-1/2 border-t-1 rounded-t-2xl" />
+                    <div className="bg-white p-2 border-b-0 rounded-b-2xl">
+                        <h1 className="text-black font-semibold pt-3">{val.title}</h1>
+                        <span className="flex justify-start items-center pt-2">
+                            <FaUser />
+                            <p className="text-left text-xl font-semibold pl-2">{val.author}</p>
+                        </span>
+                        <p className="py-2">Published on : {val.published_date}</p>
+                        <Link to="/Blogs">Read more....</Link>
+                    </div>
+                </div>
+            ))}
         </>
-    )
+    );
 }
 
+export default Cards;
 
 
-export default Cards
+
+
+
